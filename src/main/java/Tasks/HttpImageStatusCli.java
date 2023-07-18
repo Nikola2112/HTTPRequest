b134425c41c8b391d4b07a6bed3f9411b1d9c8e2
Nikola2112 <nikola21122004@gmail.com>
2023-07-18 15:01:42 +0100

    error of not valid number

--- src/main/java/Tasks/HttpImageStatusCli.java
+++ src/main/java/Tasks/HttpImageStatusCli.java
@@ -1,6 +1,7 @@
 package Tasks;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HttpImageStatusCli {
@@ -13,25 +14,27 @@ public class HttpImageStatusCli {
     public void askStatus() {
         Scanner scanner = new Scanner(System.in);
 
-        try {
-            System.out.print("Enter HTTP status code: ");
-            String input = scanner.nextLine();
+        int statusCode = 0;
+        boolean isValidInput = false;
 
-            int statusCode;
+        do {
             try {
-                statusCode = Integer.parseInt(input);
-            } catch (NumberFormatException e) {
+                System.out.print("Enter HTTP status code: ");
+                statusCode = scanner.nextInt();
+                isValidInput = true;
+            } catch (InputMismatchException e) {
                 System.out.println("Please enter a valid number.");
-                return;
+                scanner.nextLine();
             }
+        } while (!isValidInput);
 
-            try {
-                imageDownloader.downloadStatusImage(statusCode);
-            } catch (IOException e) {
-                System.out.println("There is no image for HTTP status " + statusCode);
-            }
+        try {
+            imageDownloader.downloadStatusImage(statusCode);
+        } catch (IOException e) {
+            System.out.println("There is no image for HTTP status " + statusCode);
         } finally {
             scanner.close();
         }
     }
-}
+    }
+
